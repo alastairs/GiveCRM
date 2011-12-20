a8d1a085f6264cc703e514256705268d62939ebb
Alastair Smith <github@alastairsmith.me.uk>
2011-12-20 14:50:21 +0000

    Add dummy implementation of ILogger for DataGenerator to log to WPF window.

--- src/GiveCRM.DummyDataGenerator/App.xaml.cs
+++ src/GiveCRM.DummyDataGenerator/App.xaml.cs
@@ -5,6 +5,7 @@
     using GiveCRM.DummyDataGenerator.Generation;
     using Ninject;
     using Ninject.Extensions.Conventions;
+    using Ninject.Extensions.Logging;
     using Ninject.Modules;
 
     public partial class App
@@ -34,6 +35,175 @@
         {
             Action<string> logAction = logString => { };
             Kernel.Bind<Action<string>>().ToMethod(_ => logAction);
+
+            Kernel.Bind<ILogger>().To<WpfControlLogger>();
+        }
+    }
+
+    public class WpfControlLogger : ILogger
+    {
+        /// <summary>
+        /// Logs the specified message with Debug severity.
+        /// </summary>
+        /// <param name="format">The message or format template.</param><param name="args">Any arguments required for the format template.</param>
+        public void Debug(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Logs the specified exception with Debug severity.
+        /// </summary>
+        /// <param name="exception">The exception to log.</param><param name="format">The message or format template.</param><param name="args">Any arguments required for the format template.</param>
+        public void Debug(Exception exception, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Logs the specified message with Info severity.
+        /// </summary>
+        /// <param name="format">The message or format template.</param><param name="args">Any arguments required for the format template.</param>
+        public void Info(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Logs the specified exception with Info severity.
+        /// </summary>
+        /// <param name="exception">The exception to log.</param><param name="format">The message or format template.</param><param name="args">Any arguments required for the format template.</param>
+        public void Info(Exception exception, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Logs the specified message with Trace severity.
+        /// </summary>
+        /// <param name="format">The message or format template.</param><param name="args">Any arguments required for the format template.</param>
+        public void Trace(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Logs the specified exception with Trace severity.
+        /// </summary>
+        /// <param name="exception">The exception to log.</param><param name="format">The message or format template.</param><param name="args">Any arguments required for the format template.</param>
+        public void Trace(Exception exception, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Logs the specified message with Warn severity.
+        /// </summary>
+        /// <param name="format">The message or format template.</param><param name="args">Any arguments required for the format template.</param>
+        public void Warn(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Logs the specified exception with Warn severity.
+        /// </summary>
+        /// <param name="exception">The exception to log.</param><param name="format">The message or format template.</param><param name="args">Any arguments required for the format template.</param>
+        public void Warn(Exception exception, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Logs the specified message with Error severity.
+        /// </summary>
+        /// <param name="format">The message or format template.</param><param name="args">Any arguments required for the format template.</param>
+        public void Error(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Logs the specified exception with Error severity.
+        /// </summary>
+        /// <param name="exception">The exception to log.</param><param name="format">The message or format template.</param><param name="args">Any arguments required for the format template.</param>
+        public void Error(Exception exception, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Logs the specified message with Fatal severity.
+        /// </summary>
+        /// <param name="format">The message or format template.</param><param name="args">Any arguments required for the format template.</param>
+        public void Fatal(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Logs the specified exception with Fatal severity.
+        /// </summary>
+        /// <param name="exception">The exception to log.</param><param name="format">The message or format template.</param><param name="args">Any arguments required for the format template.</param>
+        public void Fatal(Exception exception, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the type associated with the logger.
+        /// </summary>
+        public Type Type
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether messages with Debug severity should be logged.
+        /// </summary>
+        public bool IsDebugEnabled
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether messages with Info severity should be logged.
+        /// </summary>
+        public bool IsInfoEnabled
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether messages with Trace severity should be logged.
+        /// </summary>
+        public bool IsTraceEnabled
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether messages with Warn severity should be logged.
+        /// </summary>
+        public bool IsWarnEnabled
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether messages with Error severity should be logged.
+        /// </summary>
+        public bool IsErrorEnabled
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether messages with Fatal severity should be logged.
+        /// </summary>
+        public bool IsFatalEnabled
+        {
+            get { throw new NotImplementedException(); }
         }
     }
 }
